5f493942d97c34ca3f9432a066ac1b4cb2409465
Neeraj kaushik <168729736+Neerajkaushik7@users.noreply.github.com>
2026-09-22 23:43:25 +0530

    Add files via upload

---
+++ Function/Sqr_usingLambda_.py
@@ -0,0 +1,16 @@
+"""
+Program Name: Square of a Number Using Lambda
+Author: Neeraj Kaushik
+Description: Demonstrates the use of a lambda function to calculate the square of a number.
+
+# Implementation:
+"""
+
+# Lambda Function
+square = lambda x: x * x
+
+# User Input
+num = int(input("Enter a number: "))
+
+# Display Result
+print("Square:", square(num))
